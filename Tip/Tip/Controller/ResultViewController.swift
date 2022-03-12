b62d21fea15dc77e5d2eac9bfeac33d025f4018b
omer <omerbzblt613@gmail.com>
2022-03-13 00:42:43 +0300

    First screen completed.

---
+++ Tip/Tip/Controller/ResultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultViewController.swift
+//  Tip
+//
+//  Created by omer faruk bozbulut on 13.03.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Tip/Tip/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by omer faruk bozbulut on 12.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var calculateButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculateButton.layer.cornerRadius = 10.0
+    }
+
+
+}
+
